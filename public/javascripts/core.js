0bcc4b8091b1eebbeb85bc24595ad28a7fb2ee1b
Atomsk1988 <braisva@hotmail.com>
2014-01-22 20:15:35 +0100

    init

---
+++ public/javascripts/core.js
@@ -0,0 +1,196 @@
+// public/core.js
+
+var admin = angular.module('admin', ['ngRoute'])
+.service('dataService', ['$http', function($http) {
+
+
+	var url = 'http://localhost:3000/';
+	var base_data = [];
+	base_data['discos'] = null;
+
+	$http({
+		method: 'GET',
+		url: url + 'discos'
+	}).success(function(data) {
+		base_data['discos'] = data;
+	});
+	console.log(base_data['discos'])
+
+	this.getDiscos = function(){
+		promise = $http({
+			method: 'GET',
+			url: url + 'discos'
+		}).success(function(data) {
+			this.discos = data;
+			return {'data':data};
+		});
+		return promise;
+	}
+	this.insertDisco = function(ob){
+		promise = $http({
+			method: 'POST',
+			url: url + 'discos',
+			data:{data:ob.data, image:ob.image}
+		}).success(function(msg) {
+			console.log('amo a vé1')	
+			//discos = data;
+			//return {'data':data};
+			return msg.data;
+		});
+		return promise;
+	}
+	this.updateDisco = function(ob){
+		promise = $http({
+			method: 'POST',
+			url: url + 'discos',
+			data:{data:ob.data}
+		}).success(function(msg) {
+			console.log('amo a vé2')	
+			//discos = data;
+			//return {'data':data};
+			return msg.data;
+		});
+		return promise;
+		//Publico un post para actualizar
+	}
+	this.getDiscosList = function(){
+		console.log(discos)
+		return data['discos'];
+	}
+	this.getList = function(list){
+		//return base_data[list];
+		result = [];
+		//console.log(list, base_data[list])
+		if(list == 'discos'){
+			for(var item in base_data[list]){
+				result.push({_id:base_data[list][item]._id, email:base_data[list][item].email, name:base_data[list][item].name})
+			}
+			return result;
+		}
+	}
+	//
+}])
+.controller('adminController', ['$scope', '$http', 'dataService',  function($scope, $http, data){
+	$scope.discos = 'discos';
+}])
+.directive('datalist', function(){
+	return {
+		restrict:'A',
+		transclude:true,
+		templateUrl: 'partials/datalist.html',
+		scope: {
+	      originData: '=origin'
+	    },
+		controller: ['$scope', '$http', 'dataService',  function($scope, $http, dataService) {
+			//console.log($scope.originData);
+		    $scope.d = dataService.getList($scope.originData);
+			$scope.selected = null;//Selected field array id
+
+			$scope.getSelectedData = function(){
+				if($scope.selected!=null)return $scope.d[$scope.selected];
+			}
+			$scope.getSelectedId = function(){
+				return $('.table_highlight').attr('id').substr(6);
+			}
+	    }],
+	    link: function(scope, element, attrs){
+
+	    }	
+	}
+})
+.directive('objecteditor', function(){
+	return{
+		restrict:'A',
+		//transclude:true,
+		require: '^datalist',
+		scope: {
+	      ngModel: '='
+	    },
+		templateUrl: 'partials/ob-editor.html',
+		controller:['$scope', '$http', 'dataService', function($scope, $http, dataService){
+			$scope.user = {_id:'', name:'', email:''};
+
+			$scope.insert = function(user,action){
+				console.log(action);
+				/*
+				if($(element).children().children('input[type=hidden]').attr('val')==''){
+					scope.insert();
+				}else{
+					scope.update();
+				}
+				*/
+				if(action=='save'){
+					dataService.insertDisco({data:$scope.user, image:$scope.user.image}).then(function(res){
+						$scope.$parent.d.push({_id:res.data._id, name:res.data.name, email:res.data.email});
+						$scope.user._id = res.data._id;
+						$('#fileupload').fileupload({
+							formData: {id:res.data._id,type:'disco'}
+						})
+						//$($scope.$element).children().children('img').attr('src', 'uploads/disco/'+scope.$parent.getSelectedData()._id+'.jpg');
+					});
+				}else if(action=='update'){
+					dataService.updateDisco({data:$scope.user})
+				}
+				//Aqui elijo si update o insert y llamo al service
+			}
+			$scope.update = function(){
+				dataService.updateDisco({data:$scope.user, _id:$scope.user._id}).then(function(res){
+					//busco el key de _id en array y lo actualizo
+					//scope.$parent.d.indexOf(res.data._id)
+				});
+			}
+			$scope.reset = function(){
+				$scope.user = {_id:'', name:'', email:''};
+				$('div[origin="discos"] img').attr('src','');
+				$('input').val('');
+				$('#fileupload').fileupload({
+					formData: {id:$scope.user._id,type:'disco'}
+				})
+			};
+			$scope.setData =function(ob){
+				console.log('setdata');
+				//console.log(ob);
+				//$scope.user = ob;
+				$scope.$parent.selected = $scope.$parent.getSelectedId();
+				$scope.$apply(function () {
+					$scope.user = $scope.$parent.getSelectedData();
+			        //scope.time = element.datepicker('getDate');
+			    });
+			    $('#fileupload').fileupload({
+					formData: {id:$scope.user._id,type:'disco'}
+				})
+		        console.log($scope.user._id,'u.u');
+
+				console.log($scope.$parent.getSelectedData());
+			}
+			$scope.reset();
+		}],
+		link:function(scope, element, attrs, dataListCtrl){
+			$(element).parent().children('table').children('tbody').children('tr').bind('click',  function(e){
+
+				if(e.currentTarget==$(element).parent().children('table').children('tbody').children('tr.table_highlight')[0]){
+					console.log('equal');
+					$(element).parent().children('table').children('tbody').children('tr.table_highlight').removeClass('table_highlight');
+					scope.reset();
+				}else{
+					console.log('not equal');
+					$(element).parent().children('table').children('tbody').children('tr.table_highlight').removeClass('table_highlight');
+					$(e.currentTarget).addClass('table_highlight');
+					scope.setData();
+					$(element).children().children('img').attr('src', 'uploads/disco/'+scope.$parent.getSelectedData()._id+'.jpg');
+				}
+			});
+
+
+			$('#fileupload').fileupload({
+			    url: '/imageUpload',
+			    type: 'POST',
+			    singleFileUploads: true,
+				formData: {id:scope.user._id,type:'disco'},
+				done: function(e,data){
+					$(e.target).parent().children('img').attr('src','.'+data.result.substr(8));
+				}
+			});
+		}
+	}
+});
---
+++ routes/index.js
@@ -0,0 +1,169 @@
+/*
+ * GET home page.
+ */
+
+exports.discos = function(db){
+    return function(req,res){
+        var collection = db.get('discos');
+
+        collection.find({}, function (err, docs){
+                console.log(err,docs.length+' results')
+                if(err==null){
+                    res.json(docs);
+                }else{
+                    res.send("error");
+                }
+            }
+        );
+    }
+}
+exports.insertDisco = function(db){
+    return function(req, res){
+        var collection = db.get('discos');
+        var newdata = req.param('data');
+        console.log(newdata);
+        if(newdata._id==null){
+            console.log('insert');
+            //INSERT
+            newdata._id = null
+            newdata.image = null;
+            console.log(newdata);
+            collection.insert(newdata, function(err, inserted){
+                res.json(inserted);
+            });
+        }else{
+            console.log('update');
+            //UPDATE
+            collection.update({_id:newdata._id}, newdata, function(err, inserted){
+                res.json(inserted);
+            });
+
+        }
+        
+        
+        /*
+        temp_ob = new Disco(req.param('data'));
+        collection.save(function(error, Disco){
+            console.log(Disco);
+
+            res.json(ob);
+        });
+        */
+        //console.log(req.param('data'));
+    }
+}
+exports.updateDisco = function(db){
+
+}
+exports.imageUpload = function(fs){
+    return function(req, res){     
+        //res.send('true');
+        fs.readFile(req.files.files[0].path, function (err, imageData) {
+            if(err)console.log(err);
+            ext = req.files.files[0].name.substr(-3);
+
+            var newPath = "./public/uploads/"+req.body.type+"/"+req.body.id+"."+ext;
+           
+            fs.writeFile(newPath, imageData, function (err) {
+                if(err){console.log(err)}else{res.send(newPath)};
+            });
+            
+        });
+        
+    }
+}
+exports.login = function(db){
+    return function(req, res){
+      res.render('index', { title: 'Noches-Bcn' });
+    };
+}
+exports.formLogin = function(db){
+    return function(req,res){
+        var user = req.body.username;
+        var password = req.body.password;
+        var collection = db.get('users');
+
+        collection.find({"username":user, "password":password}, 
+            function (err, docs){
+                console.log(err,docs.length)
+                if(err==null && docs.length==1){
+                    //res.location("main");
+                    res.redirect('main');
+                }else{
+                    res.send("Invalid login");
+                }
+            }
+        );
+    }
+}
+exports.main = function(req, res){
+    res.sendfile('./views/main.html');
+}
+/*
+exports.fiestas = function(db){
+    return function(req,res){
+        var collection = db.get('discos');
+
+        collection.find({}, function (err, docs){
+                console.log(err,docs.length+' results')
+                if(err==null){
+                    res.json(docs);
+                }else{
+                    res.send("error");
+                }
+            }
+        );
+    }
+}
+*/
+/*
+
+exports.helloworld = function(req, res){
+  res.render('helloworld', { title: 'Hello, World!' });
+};
+
+exports.userlist = function(db) {
+    return function(req, res) {
+        var collection = db.get('usercollection');
+        collection.find({},{},function(e,docs){
+            res.render('userlist', {
+                "userlist" : docs
+            });
+        });
+    };
+};
+
+
+exports.newuser = function(req, res){
+  res.render('newuser', { title: 'Add New User' });
+};
+exports.adduser = function(db) {
+    return function(req, res) {
+
+        // Get our form values. These rely on the "name" attributes
+        var userName = req.body.username;
+        var password = req.body.password;
+
+        // Set our collection
+        var collection = db.get('usercollection');
+
+        // Submit to the DB
+        collection.insert({
+            "user" : userName,
+            "password" : password
+        }, function (err, doc) {
+            if (err) {
+                // If it failed, return error
+                res.send("There was a problem adding the information to the database.");
+            }
+            else {
+                // If it worked, set the header so the address bar doesn't still say /adduser
+                res.location("userlist");
+                // And forward to success page
+                res.redirect("userlist");
+            }
+        });
+
+    }
+}
+*/
